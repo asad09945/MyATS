826002c94f26ad155f72eaafd9b1bb2538f00675
asad09945 <aejaz2067@alasu.edu>
2025-03-06 21:39:38 -0600

    Add files via upload

---
+++ MyATS/test ats.py
@@ -0,0 +1,47 @@
+import re
+from docx import Document
+import pdfplumber
+
+class ATSSimulator:
+    def __init__(self, file):
+        self.file = file
+    def simulate(self):
+        if self.file.endswith(".docx"):
+            get_text = self.get_text_doc()
+        elif self.file.endswith(".pdf"):
+                get_text=self.get_text_pdf()
+        else:
+            return "Invalid File"
+        x=len(re.findall(r"\b(python|sql|tablaeu|power bi)\b", get_text, re.IGNORECASE))
+        return x
+
+
+    def get_text_doc(self):
+        doc=Document(self.file)
+        fullText = []
+        for para in doc.paragraphs:
+            fullText.append(para.text)
+        fullText = ' '.join(fullText)
+        return fullText
+    def get_text_pdf(self):
+        with pdfplumber.open(self.file) as pdf:
+            fullText = []
+            for page in pdf.pages:
+                fullText.append(page.extract_text())
+            fullText = ' '.join(fullText)
+            return fullText
+
+
+def main():
+    file = input("Enter the file directory: ")
+    simulator = ATSSimulator(file)
+    a=simulator.simulate()
+    if a>10:
+        print("Under Review")
+    else:
+        print("Application rejected")
+if __name__ == "__main__":
+    main()
+
+
+
